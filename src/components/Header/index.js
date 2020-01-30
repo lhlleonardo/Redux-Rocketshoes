ec6f13e7a6e688ab1aa4814cc95ddbdfe2d1bb64
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2020-01-29 23:10:08 -0300

    [20200129] Initial Commit -  Add project files.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { MdShoppingBasket } from 'react-icons/md';
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 export default function Header() {
     return (
         <Container>
-            <h1>Home</h1>
+            <Link to="/">
+                <img src={logo} alt="Rocketshoes" />
+            </Link>
+            <Cart to="/cart">
+                <div>
+                    <strong>My itens</strong>
+                    <span>3 itens</span>
+                </div>
+                <MdShoppingBasket size={36} color="#fff" />
+            </Cart>
         </Container>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import { Switch, Route } from 'react-router-dom';
 import React from 'react';
 
 import Home from './Pages/Home/index';
+import Cart from './Pages/Cart/index';
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/cart" component={Cart} />
         </Switch>
     );
 }
